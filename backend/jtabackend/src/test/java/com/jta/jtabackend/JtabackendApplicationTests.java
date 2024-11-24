43d995cb611b688f79104133f3dcf8d89998498a
Dinesh Kumar Baalajee Jothi <dn466091@dal.ca>
2024-11-23 20:27:36 -0400

    Added the initial files for the backend of the application

---
+++ backend/jtabackend/src/test/java/com/jta/jtabackend/JtabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jta.jtabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
